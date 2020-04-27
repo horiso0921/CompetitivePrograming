84ef5e75284b95c3e289e836132b72479e250bb9
horiso0921 <dorink0921@gmail.com>
2020-04-27 12:55:58 +0900

    fix

--- テンプレ.py
+++ テンプレ.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 from collections import defaultdict,deque
 from heapq import heappush, heappop
-import sys, math, bisect, random, itertools
+from bisect import bisect_left, bisect_right
+import sys, random, itertools, math
 sys.setrecursionlimit(10**5)
 input = sys.stdin.readline
 sqrt = math.sqrt
-bisect_left = bisect.bisect_left
-bisect_right = bisect.bisect_right
 def LI(): return list(map(int, input().split()))
 def LF(): return list(map(float, input().split()))
 def LI_(): return list(map(lambda x: int(x)-1, input().split()))
 def II(): return int(input())
 def IF(): return float(input())
-def LS(): return list(map(list, input().split()))
-def S(): return list(input().rstrip())
+def S(): return input().rstrip()
+def LS(): return S().split()
 def IR(n): return [II() for _ in range(n)]
 def LIR(n): return [LI() for _ in range(n)]
 def FR(n): return [IF() for _ in range(n)]
@@ -24,51 +23,11 @@ def LSR(n): return [LS() for _ in range(n)]
 mod = 1000000007
 inf = float('INF')
 
-#A
-def A():
-  n, m = LI()
-  a = LI()
-  for i in range(n-1):
-    a[i + 1] += a[i]
-  a = [i % m for i in a]
-  d = defaultdict(int)
-  for i in range(n):
-    d[a[i]] += 1
-  ans = d[0]
-  for i in d.values():
-    ans += i * (i-1) // 2
-  print(ans)
-
+#solve
+def solve():
   return
 
-#B
-def B():
-  return
 
-#C
-def C():
-  return
-
-#D
-def D():
-  return
-
-#E
-def E():
-  return
-
-#F
-def F():
-  return
-
-#G
-def G():
-  return
-
-#H
-def H():
-  return
-
-#Solve
+#main
 if __name__ == '__main__':
-  A()
+  solve()
