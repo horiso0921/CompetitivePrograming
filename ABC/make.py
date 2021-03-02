e3115de1a760704694c8ab27fd181f932f0bf652
horiso0921 <horiso0921@gmail.com>
2021-03-02 12:19:15 +0900

    ADD：微調整

--- ABC/make.py
+++ ABC/make.py
@@ -3,6 +3,6 @@ while 1:
     i = int(input(">"))
     if i == -1:
         break
-    make = r"C:\Users\crdjf\Desktop\Program\contest\ABC\{}".format(str(i))
+    make = r"C:\Users\horiu\Program\contest\ABC\{}".format(str(i))
     shutil.copytree(
-        r'C:\Users\crdjf\Desktop\Program\contest\ABC\template', make)
+        r'C:\Users\horiu\Program\contest\ABC\template', make)
