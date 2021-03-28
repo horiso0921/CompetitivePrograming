87bce40706edc762fc2e7af3e2bae459a0fd6c5e
horiso0921 <horiso0921@gmail.com>
2021-03-29 00:12:50 +0900

    ARC115

---
+++ ARC/115/a.py
@@ -0,0 +1,50 @@
+from collections import defaultdict,deque
+from heapq import heappush, heappop
+from bisect import bisect_left, bisect_right
+import sys, random, itertools, math
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+sqrt = math.sqrt
+mod = 1000000007
+inf = 1e10
+def LI(): return list(map(int, input().split()))
+def LF(): return list(map(float, input().split()))
+def LI_(): return list(map(lambda x: int(x)-1, input().split()))
+def II(): return int(input())
+def IF(): return float(input())
+def S(): return input().rstrip()
+def LS(): return S().split()
+def IR(n): return [II() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def FR(n): return [IF() for _ in range(n)]
+def LFR(n): return [LI() for _ in range(n)]
+def LIR_(n): return [LI_() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+
+# solve
+def solve():
+    n, m = LI()
+    s = SR(n)
+    a = []
+    for si in s:
+        tmp = 0
+        for j in range(len(si)):
+            tmp += (si[j] == "1") << j
+        a.append(tmp)
+    x = a[0]
+    odd = 0
+    even = 0
+    for ai in a:
+        tmp = x ^ ai
+        if bin(tmp).count("1") & 1:
+            odd += 1
+        else:
+            even += 1
+    print(odd * even)
+    return
+
+
+# main
+if __name__ == '__main__':
+    solve()
