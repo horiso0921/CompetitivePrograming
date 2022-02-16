666f6335e6949eb4f88db8097c9e8d32e724454b
horiso0921 <horiso0921@gmail.com>
2022-02-16 11:58:57 +0900

    Score：112125642：y=15にひたすら壁を置く

--- AHC/8/a.py
+++ AHC/8/a.py
@@ -34,11 +34,12 @@ HUMAN = 3
 VACANT = 2
 ADJACENT = [(i, j) for i in range(-1, 2)
             for j in range(-1, 2) if i * j == 0 and i != j]
-num_to_UDLR = {(-1, 0): "L", (0, -1): "U", (0, 1): "D", (1, 0): "R"}
-UDLR_to_movement = {"U": (0, -1), "D": (0, 1), "L": (-1, 0), "R": (1, 0), ".": (0, 0)}
-num_to_udlr = {(-1, 0): "l", (0, -1): "u", (0, 1): "d", (1, 0): "r"}
-udlr_to_movement = {"u": (0, -1), "d": (0, 1), "l": (-1, 0), "r": (1, 0), ".": (0, 0)}
-
+num_to_UDLR = {(0, -1): "L", (-1, 0): "U", (1, 0): "D", (0, 1): "R"}
+UDLR_to_movement = {"U": (-1, 0), "D":(1, 0),
+                    "L": (0, -1), "R": (0, 1), ".": (0, 0)}
+num_to_udlr = {(0, -1): "l", (-1, 0): "u", (1, 0): "d", (0, 1): "r"}
+udlr_to_movement = {"u": (-1, 0), "d":(1, 0),
+                    "l": (0, -1), "r": (0, 1), ".": (0, 0)}
 
 def solve():
     n = II()
@@ -47,57 +48,56 @@ def solve():
     h = LIR_(m)
     field = [[VACANT] * 30 for i in range(30)]
     for x, y, _ in p:
-        field[y][x] *= PET
+        field[x][y] *= PET
     for x, y in h:
-        field[y][x] *= HUMAN
+        field[x][y] *= HUMAN
 
-    def is_inside_field(y: int, x: int):
-        return 0 <= y < 30 and 0 <= x < 30
+    def is_inside_field(x: int, y: int):
+        return 0 <= x < 30 and 0 <= y < 30
 
-    def can_post_obst(y: int, x: int) -> bool:
-        if not is_inside_field(y, x):
+    def can_post_obst(x: int, y: int) -> bool:
+        if not is_inside_field(x, y):
             return False
-        has_obj = field[y][x] % PET == 0 or field[y][x] % HUMAN == 0 or field[y][x] == OBST
-        for my, mx in ADJACENT:
-            my += y
+        if y != 15:
+            return False
+        has_obj = field[x][y] % PET == 0 or field[x][y] % HUMAN == 0 or field[x][y] == OBST
+        for mx, my in ADJACENT:
             mx += x
-            if is_inside_field(my, mx):
-                has_obj |= field[my][mx] % PET == 0 or field[y][x] == OBST
+            my += y
+            if is_inside_field(mx, my):
+                has_obj |= field[mx][my] % PET == 0 or field[x][y] == OBST
         return not has_obj
 
-    def can_move(y: int, x: int) -> bool:
-        if not is_inside_field(y, x):
+    def can_move(x: int, y: int) -> bool:
+        if not is_inside_field(x, y):
             return False
-        return field[y][x] != OBST
-    
-    def post_obst(y: int, x: int):
-        field[y][x] = OBST
+        return field[x][y] != OBST
 
-    def move_obj(move_dir_udlr: str, y: int, x: int, obj_type: int) -> Tuple[int, int]:
-        move_y, move_x = UDLR_to_movement[move_dir_udlr]
+    def post_obst(x: int, y: int):
+        field[x][y] = OBST
+
+    def move_obj(move_dir_udlr: str, x: int, y: int, obj_type: int) -> Tuple[int, int]:
+        move_x, move_y = UDLR_to_movement[move_dir_udlr]
         move_y += y
         move_x += x
-        field[y][x] //= obj_type
-        field[move_y][move_x] *= obj_type
-        return (move_y, move_x)
+        field[x][y] //= obj_type
+        field[move_x][move_y] *= obj_type
+        return (move_x, move_y)
 
     def move_pet(s: List[str]):
         for i in range(n):
             x, y, c = p[i]
             for movement in s[i]:
-                y, x = move_obj(movement, y, x, PET)
+                x, y = move_obj(movement, x, y, PET)
             p[i] = [x, y, c]
         return
 
     def move_human(s: Dict[int, str]):
         for i, movement in s.items():
             x, y = h[i]
-            y, x = move_obj(movement, y, x, HUMAN)
+            x, y = move_obj(movement, x, y, HUMAN)
             h[i] = [x, y]
         return
-
-    def decide_human_move() -> Dict[int, str]:
-        return {}
 
     def listup_obst_post_human(movement_human_list: List[int]) -> List[str]:
         cand = {i: 1 for i in range(m)}
@@ -109,17 +109,70 @@ def solve():
                 res.append(k)
         return res
 
+    def listup_obj_should_be_post() -> List[int]:
+        res = []
+        for x in range(30):
+            if field[x][15] != OBST:
+                res.append(x)
+        return res
+
+    def decide_human_move() -> Dict[int, str]:
+        res = {}
+        yet_post = listup_obj_should_be_post()
+        # if len(yet_post) == 1:
+        #     cand = yet_post[0]
+        #     for i in range(m):
+        #         x,y = h[i]
+        #         if y == 15:
+        #             res[i] = "L"
+        #             continue
+        #         if y > 16:
+        #             res[i] = "L"
+        #             continue
+        #         if y < 14:
+        #             res[i] = "R"
+        #             continue
+        #         if cand > x:
+        #             res[i] = "D"
+        #         elif :
+        #             res[i] = "U"
+        for i in range(m):
+            x,y = h[i]
+            if y == 15:
+                res[i] = "L"
+                continue
+            if y > 16:
+                res[i] = "L"
+                continue
+            if y < 14:
+                res[i] = "R"
+                continue
+
+            tmp = [-1, INF]
+            for post_cand in yet_post:
+                distance = abs(y - 15) + abs(x - post_cand)
+                if distance < tmp[-1]:
+                    tmp = [post_cand, distance]
+            if tmp[-1] == INF:
+                continue
+            if tmp[-1] != 1:
+                if tmp[0] > x:
+                    res[i] = "D"
+                elif tmp[0] < x:
+                    res[i] = "U"
+        return res
+
     def decide_obst_post(human_list: List[int]) -> Dict[int, str]:
         res = {}
         for i in human_list:
             x, y = h[i]
-            for myx in ADJACENT:
-                my, mx = myx
-                my += y
+            for mxy in ADJACENT:
+                mx, my = mxy
                 mx += x
-                if is_inside_field(my, mx) and can_post_obst(my, mx):
-                    post_obst(my, mx)
-                    res[i] = num_to_udlr[myx]
+                my += y
+                if is_inside_field(mx, my) and can_post_obst(mx, my):
+                    post_obst(mx, my)
+                    res[i] = num_to_udlr[mxy]
                     break
             else:
                 res[i] = "."
