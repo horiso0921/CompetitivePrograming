b6adb5a6201d9b935c288bcdb0cee1cf60ea56db
horiso0921 <horiso0921@gmail.com>
2022-03-07 14:49:49 +0900

    Score：12323046208：x=15||y=15に柵設置して最小ペット象限に1人以外入る

--- AHC/8/a.py
+++ AHC/8/a.py
@@ -2,6 +2,7 @@ from collections import defaultdict, deque
 from email.mime import base
 from heapq import heappush, heappop
 from bisect import bisect_left, bisect_right
+from tkinter.messagebox import NO
 from typing import Dict, List, Tuple
 from random import randint
 
@@ -43,13 +44,21 @@ num_to_udlr = {(0, -1): "l", (-1, 0): "u", (1, 0): "d", (0, 1): "r"}
 udlr_to_movement = {"u": (-1, 0), "d": (1, 0),
                     "l": (0, -1), "r": (0, 1), ".": (0, 0)}
 
-FIRST_PHASE = "First"  # 上に並ぶ
-SECOND_PHASE = "Second"  # 縦に並べる
-THIRD_PHASE = "Third"  # 中央に向かう
-FOURTH_PHASE = "Fourth"  # 中央に並べる
-FIFTH_PHASE = "FIFTH"  # 最後の一個を置く位置に行く
-SIXTH_PHASE = "SIXTH"  # 最後の一個を置く
+"""
+目標：
+1. x=15,y=15の全部に柵を設置 正し{(x, y):x∊{15}, y∊{14,15,16}には設置しない}
+2. みんな(x,y)=(15,15)に集合
+3. 一番ペットが少ない左右に所属できるように柵設置
+3. 一番ペットが少ない上下に所属できるように柵設置
+"""
 
+FIRST_PHASE = "First"  # x=15,y=15の全部に柵を設置
+SECOND_PHASE = "Second"  # みんな(x,y)=(15,15)に集合
+THIRD_PHASE = "Third"  # 一番ペットが少ない上下に所属できるように柵設置
+FOURTH_PHASE = "Fourth" # 一番ペットが少ない左右に所属できるように柵設置
+FIFTH_PHASE = "Fifth" # 待機
+
+NO_POSTER_NUM = 2
 
 def solve():
     n = II()
@@ -69,18 +78,16 @@ def solve():
         return 0 <= x < 30 and 0 <= y < 30
 
     def can_post_phase():
-        return phase[0] == SECOND_PHASE or phase[0] == FOURTH_PHASE or phase[0] == SIXTH_PHASE
+        return phase[0] == FIRST_PHASE or phase[0] == THIRD_PHASE or phase[0] == FOURTH_PHASE
 
-    def should_post_obst(x: int, y: int, index: int) -> bool:
-        if phase[0] == SECOND_PHASE:
-            return y == index * post_base[0]
+    def should_post_obst(x: int, y: int, index: int=0) -> bool:
+        # print(f"#{x} {y}")
+        if phase[0] == FIRST_PHASE:
+            return (x == 15 and (y != 14 and y != 15 and y != 16)) or (y == 15 and x != 15)
+        if phase[0] == THIRD_PHASE:
+            return x == 15 and (y == 14 or y == 16)
         if phase[0] == FOURTH_PHASE:
-            res = x == 15
-            if has_obst(x, y + 1) and has_obst(x, y - 1):
-                res &= False
-            return res
-        if phase[0] == SIXTH_PHASE:
-            return x == 14 or x == 16
+            return abs(x - y) == 2 or x == y
 
     def can_post_obst(x: int, y: int, index: int) -> bool:
         if not is_inside_field(x, y):
@@ -109,55 +116,39 @@ def solve():
 
     def change_phase():
         if phase[0] == FIRST_PHASE:
-            for i in range(1, m):
-                x, y = h[i]
-                if x == 0 and abs(y - post_base[0] * i) == 1:
-                    continue
-                print(f"#{x}{y}")
-                break
-            else:
-                phase[0] = SECOND_PHASE
-        elif phase[0] == SECOND_PHASE:
-            for i in range(1, m):
-                for j in range(30):
-                    if has_obst(j, post_base[0] * i):
-                        continue
-                    break
+            for y in range(30):
+                for x in range(30):
+                    if should_post_obst(x, y):
+                        if has_obst(x, y):
+                            continue
+                        else:
+                            break
                 else:
                     continue
                 break
             else:
+                phase[0] = SECOND_PHASE
+        elif phase[0] == SECOND_PHASE:
+            for i in range(NO_POSTER_NUM, m):
+                x,y = h[i]
+                if x == 15 and y == 15: continue
+                break
+            else:
                 phase[0] = THIRD_PHASE
         elif phase[0] == THIRD_PHASE:
-            for i in range(1, m):
-                x, y = h[i]
-                if abs(x - 15) == 1:
-                    continue
-                print(f"#{x}{y}")
-                break
-            else:
+            if has_obst(15, 14) or has_obst(15, 16):
                 phase[0] = FOURTH_PHASE
         elif phase[0] == FOURTH_PHASE:
-            for i in range(1, m):
-                x, y = h[i]
-                if has_obst(x + 1, y) or has_obst(x - 1, y):
-                    break
-                if has_obst(x+1, y + 1) and has_obst(x+1, y - 1) or has_obst(x-1, y + 1) and has_obst(x-1, y - 1):
-                    continue
-                break
-            else:
+            f = 0
+            for i in [14, 16]:
+                for j in [14, 16]:
+                    if has_obst(i, j):
+                        f = 1
+            if f:
                 phase[0] = FIFTH_PHASE
-        elif phase[0] == FIFTH_PHASE:
-            for i in range(1, m):
-                x, y = h[i]
-                if has_obst(x , y-1) or has_obst(x, y+1):
-                    continue
-                break
-            else:
-                phase[0] = SIXTH_PHASE
-
 
     def post_obst(x: int, y: int):
+        print(f"#POST{x} {y}")
         field[x][y] = OBST
 
     def move_obj(move_dir_udlr: str, x: int, y: int, obj_type: int) -> Tuple[int, int]:
@@ -198,87 +189,103 @@ def solve():
     def listup_obj_should_be_post() -> List[int]:
         res = []
         for x in range(30):
-            if field[x][15] != OBST:
-                res.append(x)
+            for y in range(30):
+                if should_post_obst(x, y) and field[x][y] != OBST:
+                    res.append((x, y))
         return res
+    
+    def listup_box_should_be_moved(should_post: List[int]) -> List[int]:
+        res = []
+        for obj_box in should_post:
+            x, y = obj_box
+            for mx, my in ADJACENT:
+                mx += x
+                my += y
+                if can_move(mx, my):
+                    res.append((mx, my))
+        return res
+                    
+    def search_way_from_now_to_distination(now_x: int, now_y: int, dist_x:int, dist_y:int) -> Tuple[int, int]:
+        d = {}
+        q = [[now_x+mx, now_y+my, (mx, my)] for mx, my in ADJACENT if can_move(now_x+mx, now_y+my)]
+        # print(f"#{q}")
+        for x,y,xy in q:
+            d[(x,y)] = 1
+            if x == dist_x and y == dist_y:
+                return xy
+        for xy in q:
+            x,y,mxy = xy
+            for mx, my in ADJACENT:
+                mx += x
+                my += y
+                if can_move(mx, my) and not (mx, my) in d:
+                    if mx == dist_x and my == dist_y:
+                        return mxy
+                    d[(mx, my)] = 1
+                    nxy = [mx, my, mxy]
+                    q.append(nxy)
+        return None
 
     def decide_human_move(move_human_list: List[int]) -> Dict[int, str]:
         res = {}
+        
+        must_post = listup_obj_should_be_post()
+        cand_box = listup_box_should_be_moved(must_post)
         print(f"#{phase}")
+        # print(f"#{must_post}")
+        # print(f"#{cand_box}")
+        lis = [(0,0),(29,29),(0,29),(29,0)]
 
         for i in move_human_list:
-            x, y = h[i]
-            if i == 0:
-                mx, my = ADJACENT[1]
-                if can_move(x+mx, y+my):
-                    res[i] = num_to_UDLR[(mx, my)]
+            x,y = h[i]
+            if 0 <= i < NO_POSTER_NUM:
+                mx,my = lis[i]                
+                way = search_way_from_now_to_distination(x, y, mx, my)
+                if way == None:
+                    res[i] = "."
+                else:
+                    res[i] = num_to_UDLR[way]
+                continue
+            if phase[0] == FIRST_PHASE:
+                tmp = [500, (-1, -1)]
+                for base_x, base_y in cand_box:
+                    distance = abs(base_x - x) + abs(base_y - y)
+                    if tmp[0] > distance:
+                        tmp = [distance, (base_x, base_y)]
+                base_x, base_y = tmp[1]
+                way = search_way_from_now_to_distination(x, y, base_x, base_y)
+                if way == None:
+                    res[i] = "."
+                else:
+                    # print(f"#{way} {base_x} {base_y} {x} {y}")
+                    res[i] = num_to_UDLR[way]
+            elif phase[0] == SECOND_PHASE:
+                base_x, base_y = 15,15
+                if x == base_x and y == base_y:
+                    res[i] = "."
+                    continue
+                way = search_way_from_now_to_distination(x, y, base_x, base_y)
+                if way == None:
+                    res[i] = "."
+                else:
+                    res[i] = num_to_UDLR[way]
+            elif phase[0] == THIRD_PHASE:
+                res[i] = "."
+            elif phase[0] == FOURTH_PHASE:
+                if has_obst(15, 14):
+                    if can_move(x, y+1):
+                        res[i] = "R"
+                    else:
+                        res[i] = "."
+                elif can_move(x, y-1):
+                    res[i] = "L"
                 else:
                     res[i] = "."
+
+            elif phase[0] == FIFTH_PHASE:
+                res[i] = "."
             else:
-                if phase[0] == FIRST_PHASE:
-                    base_y = post_base[0] * i
-                    base_x = 0
-                    if abs(y - base_y) != 1:
-                        if y > base_y:
-                            res[i] = "L"
-                        else:
-                            res[i] = "R"
-                    elif x != base_x:
-                        if x > base_x:
-                            res[i] = "U"
-                    else:
-                        res[i] = "."
-                elif phase[0] == SECOND_PHASE:
-                    for mx, my in ADJACENT:
-                        mx += x
-                        my += y
-                        if is_inside_field(mx, my) and should_post_obst(mx, my, i) and has_obst(mx, my):
-                            if can_move(x+1, y):
-                                res[i] = "D"
-                            else:
-                                res[i] = "."
-                            break
-                    else:
-                        res[i] = "."
-                elif phase[0] == THIRD_PHASE:
-                    base_x = 15
-                    if abs(base_x - x) != 1:
-                        if x > base_x:
-                            res[i] = "U"
-                        else:
-                            res[i] = "D"
-                    else:
-                        res[i] = "."
-                elif phase[0] == FOURTH_PHASE:
-                    base_y = post_base[0] * i
-                    for mx, my in ADJACENT:
-                        mx += x
-                        my += y
-                        if is_inside_field(mx, my) and should_post_obst(mx, my, i) and has_obst(mx, my):
-                            if y > base_y:
-                                if can_move(x, y+1):
-                                    res[i] = "R"
-                                    break
-                            else:
-                                if can_move(x, y-1):
-                                    res[i] = "L"
-                                    break
-                    else:
-                        res[i] = "."
-                elif phase[0] == FIFTH_PHASE:
-                    base_y = post_base[0] * i
-                    base_x = 15
-                    if x > base_x:
-                        if can_move(x-1, y):
-                            res[i] = "U"
-                            continue
-                    elif x < base_x:
-                        if can_move(x+1, y):
-                            res[i] = "D"
-                            continue
-                    res[i] = "."
-                else:
-                    res[i] = "."
+                res[i] = "."
 
         return res
 
@@ -307,16 +314,27 @@ def solve():
 
         for i in human_list:
             x, y = h[i]
-            if phase[0] == SIXTH_PHASE:
-                if has_obst(x-1, y) or has_obst(x+1, y): continue
+            if phase[0] == THIRD_PHASE:
+                l_pet = clc_range_pet(x, y, x, y-1)
+                r_pet = clc_range_pet(x, y, x, y+1)
+                print(f"#{l_pet} {r_pet}")
+                if l_pet > r_pet:
+                    if can_post_obst(x, y-1, i):
+                        post_obst(x, y-1)
+                        res[i] = "l"
+                else:
+                    if can_post_obst(x, y+1, i):
+                        post_obst(x, y+1)
+                        res[i] = "r"
+            elif phase[0] == FOURTH_PHASE:
                 u_pet = clc_range_pet(x, y, x-1, y)
                 d_pet = clc_range_pet(x, y, x+1, y)
-                print(f"#{u_pet} {d_pet}")
+                print(f"#{u_pet} {d_pet}, {can_post_obst(x+1, y, i)} {can_post_obst(x-1, y, i)}")
                 if u_pet < d_pet:
                     if can_post_obst(x+1, y, i):
                         post_obst(x+1, y)
                         res[i] = "d"
-                else:
+                elif u_pet > d_pet: 
                     if can_post_obst(x-1, y, i):
                         post_obst(x-1, y)
                         res[i] = "u"
@@ -333,7 +351,7 @@ def solve():
         return res
 
     def decide_human_movement() -> List[str]:
-        post_human_dict = decide_human_post([i for i in range(1, m)])
+        post_human_dict = decide_human_post([i for i in range(NO_POSTER_NUM, m)])
         move_human_list = listup_move_human(list(post_human_dict.keys()))
         move_human_dict = decide_human_move(move_human_list)
         move_human(move_human_dict)
