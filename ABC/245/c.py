b5658da181f9cfcae99c3fa63213606a794d4a54
horiso0921 <horiso0921@gmail.com>
2022-04-11 12:31:54 +0900

    ABC245

---
+++ ABC/245/c.py
@@ -0,0 +1,53 @@
+#!/usr/bin/env python3
+from collections import defaultdict,deque
+from heapq import heappush, heappop
+from bisect import bisect_left, bisect_right
+import sys, itertools, math
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+sqrt = math.sqrt
+def LI(): return list(map(int, input().split()))
+def LF(): return list(map(float, input().split()))
+def LI_(): return list(map(lambda x: int(x)-1, input().split()))
+def II(): return int(input())
+def IF(): return float(input())
+def S(): return input().rstrip()
+def LS(): return S().split()
+def IR(n): return [II() for _ in range(n)]
+def LIR(n): return [LI() for _ in range(n)]
+def FR(n): return [IF() for _ in range(n)]
+def LFR(n): return [LI() for _ in range(n)]
+def LIR_(n): return [LI_() for _ in range(n)]
+def SR(n): return [S() for _ in range(n)]
+def LSR(n): return [LS() for _ in range(n)]
+mod = 1000000007
+inf = 1e10
+
+#solve
+def solve():
+    n,k = LI()
+    a = LI()
+    b = LI()
+    pre = [a[0], b[0]]
+    for i in range(1, n):
+        ai = a[i]
+        bi = b[i]
+        nex = set()
+        for p in pre:
+            if abs(ai-p) <= k:
+                nex.add(ai)
+            if abs(bi-p) <= k:
+                nex.add(bi)
+        pre = list(nex)
+        if pre == []:
+            print("No")
+            return
+    print("Yes")
+    return
+    
+    return
+
+
+#main
+if __name__ == '__main__':
+    solve()
